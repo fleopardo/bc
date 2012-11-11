01804e17273404ad41b34533abac63934229ba7d
fleopardo <fleopardo@gmail.com>
2012-11-11 16:01:13 -0300

    seccion novedades

---
+++ js/novedades.js
@@ -0,0 +1,20 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+*/
+
+;(function(window){
+
+	/*
+	 * Variables
+	*/
+	var boxDestacados = $(".destacado .modulos > div");
+
+ 	/*
+	 * Mismo alto para todas las cajas
+	*/
+	ypf.equalHeight(boxDestacados);
+
+
+})(window);
