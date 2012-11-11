5ccea49192957da76ca225ebe6b2283a331c27ea
fleopardo <fleopardo@gmail.com>
2012-11-11 16:56:20 -0300

    seccion novedades

--- js/novedades.js
+++ js/novedades.js
@@ -9,12 +9,48 @@
 	/*
 	 * Variables
 	*/
-	var boxDestacados = $(".destacado .modulos > div");
+	var boxDestacados = $(".destacado .modulos .info");
 
  	/*
 	 * Mismo alto para todas las cajas
 	*/
 	ypf.equalHeight(boxDestacados);
+
+
+	/*
+	 * Variables
+	*/
+	var $acordeon = $(".acordeon");
+
+
+	if( $acordeon.length > 0){
+
+		$acordeon.each(function(){
+
+			var that = $(this);
+
+			that.bind("click",function(){
+
+				if( that.hasClass("open") ){
+
+					that.removeClass("open");
+
+					that.parent().next().slideUp();
+
+				}else{
+
+					that.addClass("open");
+
+					that.parent().next().slideDown();
+
+				}
+
+			})
+
+		})
+
+	}
+	
 
 
 })(window);
