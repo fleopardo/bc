5d11a78acb4a5dc2f7d12dd8b3decd18e3af6aa8
Santiago Leopardo <santiago.leopardo@mercadolibre.com>
2012-11-13 01:08:33 -0300

    fixes, cambios de imagenes a color

--- js/contacto.js
+++ js/contacto.js
@@ -8,7 +8,12 @@
 
 	$(window).load(function() {
 
-		$(".formulario").fadeIn();
+		/*
+		 * Necesita estar en visibility hidden desde el CSS para que el plugin de menu encuentre los campos.
+		 * En el JS, le pongo display none, le quito el hidden y hago un fadeIn. (FadeIn no funciona con visibiliity hidden)
+		*/
+		$(".formulario").hide().css("visibility","visible").fadeIn();
+
 	});
 
 
---
+++ js/informe-anual.js
@@ -0,0 +1,18 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+*/
+
+;(function(window){
+
+	/*
+	 * Variables
+	*/
+	var box = $(".descarga-pdf .box");
+
+	//Mismo alto para las columnas
+	ypf.equalHeight(box);
+
+
+})(window);
