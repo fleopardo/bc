ed644adc687bd3da86e15fd5e58914db45ac75fb
Santiago Leopardo <santiago.leopardo@mercadolibre.com>
2012-11-13 02:23:56 -0300

    cambio de nombres

---
+++ js/vocaciones-perfiles.js
@@ -0,0 +1,32 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+*/
+
+;(function(window){
+
+	$(".head h1 a").bind("click",function(e){
+
+		e.preventDefault();
+
+		if( $(this).hasClass('active') ){
+
+			return false;
+		}else{
+			$(".head h1 a").removeClass('active');
+
+			$(this).addClass("active");
+
+			var section = $(this).attr("href");
+
+			$(".extended-description").hide();
+
+			$(section).stop(true,true).fadeIn('fast');
+
+		}
+
+	})
+
+
+})(window);
