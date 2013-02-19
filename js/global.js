048efb6ac73f2a7141d15052524c51ac8d500370
Santiago Leopardo <santiago.leopardo@mercadolibre.com>
2013-02-19 00:07:20 -0300

    avances en el menu principal

--- js/global.js
+++ js/global.js
@@ -6,7 +6,13 @@
 
 ;(function(window){
 
-	var latitud = {};
+	var submenuSpeedUp = 300,
+
+		submenuSpeedDown = 500,
+
+		submenuOpen = false;
+
+		latitud = {};
 
 
 	/* Helpers */
@@ -14,6 +20,12 @@
 	latitud.$document = $(document);
 
 	latitud.$body = $("body");
+
+	/*
+	 * Elimino class no-js
+	*/
+	$("html").removeClass("no-js");
+
 
 	/*
      * Detect user agent
@@ -28,11 +40,74 @@
 
     };
 
-	/*
-	 * Elimino class no-js
-	*/
-	$("html").removeClass("no-js");
+    /*
+     * Menu implementation
+    */
+    $(".header nav > ul li a").on("click",function(event){
 
+    	event.preventDefault();
+    	event.stopPropagation();
+
+    	var that = $(this); 			// link clickeado
+    	var li = that.parent();	 		// LI padre del link clickeado
+    	var submenu = li.find("div");	// Submenu del link clickeado
+
+
+    	// Si el link ya esta activo
+    	if( li.hasClass("active") ){
+
+    		// Borro la class active
+    		li.removeClass("active");
+
+    		// Oculto el submenu
+    		submenu.stop().slideUp(submenuSpeedUp);
+
+    		// Seteo la variable en false porque no queda ninguno abierto
+    		submenuOpen = false;
+
+    	}else{
+
+	    	// Remuevo los active por si hay alguno activo
+	    	li.siblings().removeClass("active");
+
+	    	// Si no hay submenu abierto lo abro instantaneamente..
+	    	if( submenuOpen == false){
+
+    			submenu.stop(true,true).slideDown(submenuSpeedDown,function(){
+
+    				submenuOpen = true;
+
+    				li.addClass("active");
+
+    			});
+
+    		}else{
+
+	    		// Oculto los submenu por si hay otro abierto
+    			$(".submenu").stop(true,true).slideUp(submenuSpeedUp);
+
+    			// Lo abro con delay para que el submenu que este abierto llegue a retraerse y luego se abra el proximo..(Debido a que el callback no funciona como espero)
+    			setTimeout(function(){
+
+	    			li.addClass("active");
+
+	    			submenu.stop(true,true).slideDown(submenuSpeedDown,function(){
+
+	    				submenuOpen = true;
+
+	    			});
+
+	    		},submenuSpeedUp);
+
+	    	}
+
+	    }
+
+	});
+
+    /*
+     * Export object
+    */
 	window.latitud = latitud;
 
 })(window);
