75ea1c6a31d3135e86a058b87bd47813151e8196
Santiago Leopardo <santiago.leopardo@mercadolibre.com>
2013-02-25 22:05:11 -0300

    directorio libs para los css de plugins, inicializacion de scroll personalizado

---
+++ js/nuestros-vinos.js
@@ -0,0 +1,14 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+*/
+
+;(function(window){
+
+	$('.scroll').jScrollPane({
+		verticalDragMaxHeight : 39,
+		verticalDragMinHeight : 39
+	});
+
+})(window);
