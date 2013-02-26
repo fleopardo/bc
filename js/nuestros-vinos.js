0c2d72fd1b9fdb1e52978acd6ec78f59466ae1cf
Santiago Leopardo <santiago.leopardo@mercadolibre.com>
2013-02-26 10:20:49 -0300

    soporte para touch devices del scrollpane

--- js/nuestros-vinos.js
+++ js/nuestros-vinos.js
@@ -6,9 +6,40 @@
 
 ;(function(window){
 
-	$('.scroll').jScrollPane({
+	var $scrollCustom = $('.scroll');
+
+	// Inicializacion jsScrollPane
+	$scrollCustom.jScrollPane({
 		verticalDragMaxHeight : 39,
 		verticalDragMinHeight : 39
 	});
+
+	// jsScrollPane soporte tactil
+
+	if(latitud.isMobile()){
+
+ 		$scrollCustom.bind('touchstart', function(e){
+			var cpos = dragPosition;
+
+			e = e.originalEvent.touches[0];
+
+			var sY = e.pageY;
+			var sX = e.pageX;
+
+			$scrollCustom.bind('touchmove',function(ev){
+				ev.preventDefault();
+				ev = ev.originalEvent.touches[0];
+
+				var top = cpos-(ev.pageY-sY);
+				positionDrag(top);
+
+			});
+
+			$scrollCustom.bind('touchend',function(ev){
+				$scrollCustom.unbind('touchmove touchend');
+			});
+
+		});
+ 	}
 
 })(window);
