cbe02188432b35d7108b7a7d80ceee7a9c8c6d68
fleopardo <fleopardo@gmail.com>
2013-03-15 12:43:09 -0300

    se sube cepas

--- js/cepas.js
+++ js/cepas.js
@@ -6,7 +6,47 @@
 
 ;(function(window){
 
+	$scrollCustom = $('.scroll'),
 
+	/*
+ 	 * Inicializacion jsScrollPane
+ 	*/
+	$scrollCustom.jScrollPane({
+		verticalDragMaxHeight : 39,
+		verticalDragMinHeight : 39,
+		setWheelScrollingEnabled : true
+	});
+
+	/*
+ 	 * jsScrollPane soporte tactil
+ 	*/
+
+	if(latitud.touch){
+
+ 		$scrollCustom.bind('touchstart', function(e){
+
+			var cpos = dragPosition;
+
+			e = e.originalEvent.touches[0];
+
+			var sY = e.pageY;
+			var sX = e.pageX;
+
+			$scrollCustom.bind('touchmove',function(ev){
+				ev.preventDefault();
+				ev = ev.originalEvent.touches[0];
+
+				var top = cpos-(ev.pageY-sY);
+				positionDrag(top);
+
+			});
+
+			$scrollCustom.bind('touchend',function(ev){
+				$scrollCustom.unbind('touchmove touchend');
+			});
+
+		});
+ 	}
 	
 
 })(window);
