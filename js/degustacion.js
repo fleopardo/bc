842301ebcd048c2291436e488d6dfba5f6aca67d
fleopardo <fleopardo@gmail.com>
2013-03-29 18:24:12 -0300

    se agregan cosas a degustacion

--- js/degustacion.js
+++ js/degustacion.js
@@ -28,4 +28,15 @@
 
 	})
 
+	$('.olfativa dt').each(function(){
+ 		$(this).click(function(){
+ 			$(this).parent().siblings('dl').find('dd').slideUp();
+ 			$(this).parent().siblings('dl').find('dt').removeClass('active');
+ 			$(this).toggleClass('active');
+ 			$(this).siblings('dd').slideToggle();
+ 		});
+ 	});
+
+ 	
+
 })(window);
