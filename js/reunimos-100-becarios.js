9affa366d94a5be4aef0575bc5ebe496e1e3602b
fleopardo <fleopardo@gmail.com>
2013-07-05 11:56:36 -0300

    se actualiza con de todo

---
+++ js/reunimos-100-becarios.js
@@ -0,0 +1,19 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+*/
+
+;(function(window){
+
+	/*
+ 	 * Inicializacion jsScrollPane
+ 	*/
+	$('.scroll').jScrollPane({
+		verticalDragMaxHeight : 39,
+		verticalDragMinHeight : 39,
+		setWheelScrollingEnabled : true
+	});
+
+
+})(window);
