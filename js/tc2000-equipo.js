4a3ef16b90db3290a605bedd7940f19b35042526
fleopardo <fleopardo@gmail.com>
2013-09-11 17:57:51 -0300

    tc 2000 equipo

---
+++ js/tc2000-equipo.js
@@ -0,0 +1,22 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+ *
+*/
+
+
+;(function(){
+
+
+	$.backstretch("./css/assets/fd-notas.jpg");
+
+
+
+	/* Carousel de recomendados */
+	$relatedCarousel = $("#carousel-equipo");
+	$relatedCarousel.removeClass("mask").jcarousel();
+
+	
+
+}());
