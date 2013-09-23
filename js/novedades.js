c61d2978b439b5c5e330c51cc488b987b2df889e
fleopardo <fleopardo@gmail.com>
2013-09-23 15:56:30 -0300

    novedades

--- js/novedades.js
+++ js/novedades.js
@@ -10,9 +10,12 @@
 
 	var $galleryCarousel = $('#carousel-thumbs'),
 		$notasPreview = $('.bloques-tabs article'),
-		$notas = $('.notas-content');
+		$notas = $('.notas-content'),
+		$altoNotaContentInicial = $notas.find('.active').height();
 
+	$notas.css('height',$altoNotaContentInicial);
 
+	
 	$notasPreview.on("click", function(event){
 
 		 event.preventDefault();
@@ -23,12 +26,16 @@
 
 		 }else{
 
-		 	var $notaActive = $(this).data('id');
+		 	var $notaActive = $(this).data('id'),
+		 		$altoNotaActive = $notas.find('article[data-id="' + $notaActive + '"]').height();
+
+		 	console.log($altoNotaActive);
 
 		 	$notasPreview.removeClass('active');
 		 	$(this).addClass('active');
 
 		 	$notas.find('article').fadeOut();
+		 	$notas.css('height',$altoNotaActive);
 		 	$notas.find('article[data-id="' + $notaActive + '"]').fadeIn();
 		 }
 
