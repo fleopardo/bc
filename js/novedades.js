2b4ee3c43cd1ada14f2b3568b9875bc035c8564e
fleopardo <fleopardo@gmail.com>
2013-09-23 15:39:08 -0300

    novedades

--- js/novedades.js
+++ js/novedades.js
@@ -8,8 +8,32 @@
 
 ;(function(){
 
+	var $galleryCarousel = $('#carousel-thumbs'),
+		$notasPreview = $('.bloques-tabs article'),
+		$notas = $('.notas-content');
 
-	var $galleryCarousel = $('#carousel-thumbs');
+
+	$notasPreview.on("click", function(event){
+
+		 event.preventDefault();
+
+		 if( $(this).hasClass('active') ){
+
+		 	return false;
+
+		 }else{
+
+		 	var $notaActive = $(this).data('id');
+
+		 	$notasPreview.removeClass('active');
+		 	$(this).addClass('active');
+
+		 	$notas.find('article').fadeOut();
+		 	$notas.find('article[data-id="' + $notaActive + '"]').fadeIn();
+		 }
+
+
+	});
 
 
 	/* Instancio carousel */
