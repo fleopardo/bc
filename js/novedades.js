be744bc8dc19d75f37661cb3203b7e8e0b44e7e5
fleopardo <fleopardo@gmail.com>
2013-09-23 14:06:20 -0300

    novedades

---
+++ js/novedades.js
@@ -0,0 +1,20 @@
+/*
+ * autors:
+ * Santiago Leopardo @sleopardo
+ * Fernando Leopardo @fer_leopardo
+ *
+*/
+
+
+;(function(){
+
+
+	var $galleryCarousel = $('#carousel-thumbs');
+
+
+	/* Instancio carousel */
+	$galleryCarousel.removeClass("mask").jcarousel();
+
+
+
+}());
