a58d4b44a84205e9a139e1e51049492e67ad7160
sleopardo <santiagoleopardo@gmail.com>
2014-01-28 21:47:31 -0300

    Informacion financiera

--- js/informacion-financiera.js
+++ js/informacion-financiera.js
@@ -6,4 +6,53 @@
  	ypf.equalHeight($('.bloques-container.bottom .bloque > div'));
  	ypf.equalHeight($('.bloques-container.bottom .bloque'));
 
+ 		/*
+	 * Variables
+	*/
+	var $acordeon = $(".acordeon-container .titulo"),
+	    $tabs = $(".tabs li");
+
+
+	if( $acordeon.length > 0){
+
+	        $acordeon.each(function(){
+
+	                var that = $(this);
+
+	                that.bind("click",function(){
+
+	                        if( that.find(".acordeon").hasClass("open") ){
+
+	                                that.find(".acordeon").removeClass("open");
+
+	                                that.next().slideUp();
+
+	                        }else{
+
+	                                that.find(".acordeon").addClass("open");
+
+	                                that.next().slideDown();
+
+	                        }
+
+	                })
+
+	        })
+
+	}
+
+
+	if( $tabs.length > 0 ){
+
+	    $tabs.on("click", function(){
+
+	        $tabs.removeClass("active");
+	        $(this).addClass("active");
+	        $(".acordeon-container").hide();
+	        $(".acordeon-container[data-year='" + $(this).data("year") + "']").show()
+
+	    });
+
+	}
+
 })();
