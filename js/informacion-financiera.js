2a34ac373cd0b0eeffd7ce4129a0a19a4244483a
sleopardo <santiago.leopardo@mercadolibre.com>
2014-01-29 18:26:23 -0300

    funcionamiento de selects en informacion financiera

--- js/informacion-financiera.js
+++ js/informacion-financiera.js
@@ -6,7 +6,7 @@
  	ypf.equalHeight($('.bloques-container.bottom .bloque > div'));
  	ypf.equalHeight($('.bloques-container.bottom .bloque'));
 
- 		/*
+ 	/*
 	 * Variables
 	*/
 	var $acordeon = $(".acordeon-container .titulo"),
@@ -54,5 +54,24 @@
 	    });
 
 	}
+
+
+	var selectForm20 = $(".select-form20"),
+		selectInformeAnual = $(".select-informe-anual");
+
+
+ 	selectForm20.on("change", function(event){
+
+ 		$(".docs-form20").hide();
+        $(".docs-form20[data-year=" + $(this).find('option:selected').data('year') + "]").show();
+
+ 	});
+
+ 	selectInformeAnual.on("change", function(event){
+
+ 		$(".docs-informe-anual").hide();
+        $(".docs-informe-anual[data-year=" + $(this).find('option:selected').data('year') + "]").show();
+
+ 	});
 
 })();
