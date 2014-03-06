6270f63e1feb1176d644ade00d14001ac6f562ec
sleopardo <santiago.leopardo@mercadolibre.com>
2014-03-06 20:21:59 -0300

    creacion de idiomas

---
+++ es/js/informacion-financiera.js
@@ -0,0 +1,77 @@
+
+;(function(){
+
+ 	ypf.equalHeight($('.bloques-container.top .bloque > div'));
+ 	ypf.equalHeight($('.bloques-container.top .bloque'));
+ 	ypf.equalHeight($('.bloques-container.bottom .bloque > div'));
+ 	ypf.equalHeight($('.bloques-container.bottom .bloque'));
+
+ 	/*
+	 * Variables
+	*/
+	var $acordeon = $(".acordeon-container .titulo"),
+	    $tabs = $(".tabs li"),
+	    selectForm20 = $(".select-form20"),
+		selectInformeAnual = $(".select-informe-anual");
+
+
+	if( $acordeon.length > 0){
+
+	        $acordeon.each(function(){
+
+	                var that = $(this);
+
+	                that.bind("click",function(){
+
+	                        if( that.find(".acordeon").hasClass("open") ){
+
+	                                that.find(".acordeon").removeClass("open");
+
+	                                that.next().slideUp();
+
+	                        }else{
+
+	                                that.find(".acordeon").addClass("open");
+
+	                                that.next().slideDown();
+
+	                        }
+
+	                })
+
+	        })
+
+	}
+
+
+	if( $tabs.length > 0 ){
+
+	    $tabs.on("click", function(){
+
+	        $tabs.removeClass("active");
+	        $(this).addClass("active");
+	        $(".acordeon-container").hide();
+	        $(".acordeon-container[data-year='" + $(this).data("year") + "']").show()
+
+	    });
+
+	}
+
+
+	/* Selects */
+
+ 	selectForm20.on("change", function(event){
+
+ 		$(".docs-form20").hide();
+        $(".docs-form20[data-year=" + $(this).find('option:selected').data('year') + "]").show();
+
+ 	});
+
+ 	selectInformeAnual.on("change", function(event){
+
+ 		$(".docs-informe-anual").hide();
+        $(".docs-informe-anual[data-year=" + $(this).find('option:selected').data('year') + "]").show();
+
+ 	});
+
+})();
