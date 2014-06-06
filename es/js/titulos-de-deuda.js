d19c2d6af68b65c22e467822048931920e2dd927
sleopardo <santiago.leopardo@mercadolibre.com>
2014-06-06 00:34:29 -0300

    ajustes en titulos de deuda

--- es/js/titulos-de-deuda.js
+++ es/js/titulos-de-deuda.js
@@ -1,21 +1,15 @@
+$('.headerColapsible').click(function(){
+	if ( $(this).hasClass('opened') ) {
+		$(this).removeClass('opened');
+		$(this).addClass('closed');
 
-	$('.headerColapsible').click(function(){
-		if ( $(this).hasClass('opened') ) {
-			$(this).removeClass('opened');
-			$(this).addClass('closed');
+	} else if ($(this).hasClass('closed')){
+		$(this).removeClass('closed');
+		$(this).addClass('opened');
+	}
 
-		} else if ($(this).hasClass('closed')){
-			$(this).removeClass('closed');
-			$(this).addClass('opened');
-		}
+	$(this).nextUntil('tr.anchor').slideToggle(100, function(){
 
-		$(this).nextUntil('tr.anchor').slideToggle(100, function(){
+    });
 
-	    	});
-
-	});
-
-
-
-
-
+});
