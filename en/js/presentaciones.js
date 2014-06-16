56d782438020eedd63422942216b065d964d5556
Fernando <fleopardo@gmail.com>
2014-06-16 15:32:14 -0300

    Create presentaciones.js
    nuevo presentation.js para ingles

---
+++ en/js/presentaciones.js
@@ -0,0 +1,12 @@
+;(function(){
+
+ 	var select = $(".select-year");
+
+ 	select.on("change", function(event){
+
+ 		$(".docs").hide();
+        $(".docs[data-year=" + $(this).find('option:selected').data('year') + "]").show();
+
+ 	});
+
+})();
