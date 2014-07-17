417bde4eadd37cca975d67e736b7f98576132881
sleopardo <santiago.leopardo@mercadolibre.com>
2014-07-17 19:58:32 -0300

    submenu expandible, agregado de reserva de turnos, historia clinica y guia de lubricacion

--- js/global.js
+++ js/global.js
@@ -96,6 +96,50 @@
 	// 	$(this).find('.over-img').stop(true,true).fadeOut();
 	// });
 
+	// menu lateral colapsible
+
+	var aside = $('aside.left'),
+	linksExpandables;
+
+	if (aside) {
+		// listado de links que pueden ser expandibles
+		linksExpandables = $('aside.left > ul > li > a');
+
+		// Los recorro para ver cual tiene submenu y le agrego clases
+		$.each(linksExpandables, function (i,e) {
+			var $link = $(e),
+				$submenu = $link.next();
+
+			if ($submenu.length > 0) {
+				$link.addClass('expandable');
+
+				// Si el link activo esta en un submenu lo muestro abierto
+				if ($submenu.find('.active').length > 0) {
+					$link.addClass('opened');
+					$submenu.slideToggle();
+				}else{
+					$link.addClass('closed');
+				}
+
+			}
+		});
+
+		aside.on('click', '.expandable', function (event) {
+			var $trigger = $(event.target),
+				$submenu = $trigger.next();
+
+			// si esta abierto, se cierra..
+			if ($submenu.is(":visible")) {
+				$trigger.addClass('closed');
+				$trigger.removeClass('opened');
+			}else{
+				$trigger.removeClass('closed');
+				$trigger.addClass('opened');
+			}
+			$submenu.slideToggle();
+			event.preventDefault();
+		});
+	}
 
 	/*INCIALIZACION DE PLUG IN SELECTMENU*/
 	if($("select").length > 0){
