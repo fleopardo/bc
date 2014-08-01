0cadfb6dc3f8a22952ab56e24999749fcc9030b4
sleopardo <santiago.leopardo@mercadolibre.com>
2014-08-01 00:42:46 -0300

    datepicker

--- js/crear-turno.js
+++ js/crear-turno.js
@@ -9,22 +9,34 @@
 		step3 = $('.body-step3'),
 		rowsStep1 = step1.find('tr'),
 		rowsStep2 = step2.find('tr'),
-		rowsStep3 = step3.find('tr');
+		rowsStep3 = step3.find('tr'),
+		datepicker = $('#datepicker');
 
 
 	/* Funcionalidad step 1 */
+
+	$( "#datepicker" ).datepicker({
+    	monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
+        'Jul','Ago','Sep','Oct','Nov','Dic'],
+        dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mi&eacute;','Juv','Vie','S&aacute;b'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;']
+    });
 
 	// Expandos
 	$('.seleccionar-fecha').on('click', function () {
 		$('.seleccionar-horario').removeClass('active');
 		$(this).addClass('active');
 		step1.find('.seleccionar-horario-content').hide();
+		datepicker.show();
 	});
 
 	$('.seleccionar-horario').on('click', function () {
 		$('.seleccionar-fecha').removeClass('active');
 		$(this).addClass('active');
 		step1.find('.seleccionar-horario-content').show();
+		datepicker.hide();
 	});
 
 	// Rows de la tabla
