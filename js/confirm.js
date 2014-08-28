4d444db5ee162ea73aff601705baf8f541e0b4f3
sleopardo <santiagoleopardo@gmail.com>
2014-08-28 19:38:48 -0300

    confirm cancelar turno

--- js/confirm.js
+++ js/confirm.js
@@ -8,17 +8,32 @@
  *   http://www.opensource.org/licenses/mit-license.php
  */
 
-jQuery(function ($) {
+$(function ($) {
 	$('table a.mod').click(function (e) {
 		e.preventDefault();
 
 		// example of calling the confirm function
 		// you must use a callback function to perform the "yes" action
 		confirm("¿Desea modificar su turno?", function () {
-			window.location.href = 'http://www.ypf.com';
+			window.location.href = './crea-tu-turno.html';
 		});
 	});
 });
+
+
+$(function ($) {
+	$('table .cancelar a').click(function (e) {
+		e.preventDefault();
+
+		// example of calling the confirm function
+		// you must use a callback function to perform the "yes" action
+		confirm("¿Está seguro que desea cancelarlo?", function () {
+			window.location = window.location.href;
+		});
+	});
+});
+
+
 
 function confirm(message, callback) {
 	$('#confirm').modal({
