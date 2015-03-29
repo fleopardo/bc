e791865febee273f2565e56360b951f9148bcc0c
sleopardo <santiago.leopardo@mercadolibre.com>
2015-03-29 16:12:32 -0300

    menu de filtros actualizado

--- js/modules/left-menu.js
+++ js/modules/left-menu.js
@@ -2,51 +2,64 @@
 
 	// menu lateral colapsible
 
-	var aside = $('aside.principal-aside'),
-	linksExpandables;
+    var aside = $('.filtros-left'),
+        linksExpandables;
 
-	if (aside) {
-		// listado de links que pueden ser expandibles
-		linksExpandables = $('aside.principal-aside > ul > li > a');
+    if (aside) {
 
-		// Los recorro para ver cual tiene submenu y le agrego clases
-		$.each(linksExpandables, function (i,e) {
-			var $link = $(e),
-				$submenu = $link.next();
+    	// listado de links que pueden ser expandibles
+        linksExpandables = $('.filtros-left > li > label');
+        AllLabels = $('.filtros-left > li label');
 
-			if ($submenu.length > 0) {
-				$link.addClass('expandable');
+        // Los recorro para ver cual tiene submenu y le agrego clases
+        $.each(linksExpandables, function (i,e) {
 
-				// Si el link activo esta en un submenu lo muestro abierto
-				if ($submenu.find('.active').length > 0) {
-					$link.addClass('opened');
-					$submenu.slideToggle();
-				}else{
-					$link.addClass('closed');
-				}
+            var $link = $(e),
+                $submenu = $link.next();
 
-			}
-		});
+            if ($submenu.length > 0) {
+                $link.addClass('expandable closed');
+	            $submenu.slideToggle();
 
-		aside.on('click', '.expandable', function (event) {
-			var $trigger = $(event.target),
-				$submenu = $trigger.next();
+                // Si el link activo esta en un submenu lo muestro abierto
+                if ($submenu.find('.active').length > 0) {
+                    $link.addClass('opened');
+                    $submenu.slideToggle();
+                }else{
+                    $link.addClass('closed');
+                }
 
-			// si esta abierto, se cierra..
-			if ($submenu.is(":visible")) {
-				$trigger.addClass('closed');
-				$trigger.removeClass('opened');
-			}else{
-				$trigger.removeClass('closed');
-				$trigger.addClass('opened');
-			}
-			$submenu.slideToggle();
-			event.preventDefault();
-		});
-	}
+            }
+        });
 
+        aside.on('click', '.expandable', function (event) {
+            var $trigger = $(event.target),
+                $submenu = $trigger.next();
+
+            // si esta abierto, se cierra..
+            if ($submenu.is(":visible")) {
+                $trigger.addClass('closed');
+                $trigger.removeClass('opened');
+            }else{
+                $trigger.removeClass('closed');
+                $trigger.addClass('opened');
+            }
+            $submenu.slideToggle();
+            event.preventDefault();
+        });
+
+        aside.on('click', 'label', function (){
+
+        	if ($(this).find('input').prop('checked') === true){
+        		$(this).addClass('selected');
+        	} else {
+        		$(this).removeClass('selected');
+        	}
+
+        });
+
+        $('.filtros-left input[type="checkbox"]').ezMark();
+
+    }
 
 })();
-
-
-
